f3bc87cbcc24d1ac126b6e6a1869c5d0649a9a85
chenxyuhua <98442925+chenxyuhua@users.noreply.github.com>
2024-02-03 03:23:10 -0500

    UI setup

--- a_dinning_app/a_dinning_app/ContentView.swift
+++ a_dinning_app/a_dinning_app/ContentView.swift
@@ -8,14 +8,34 @@
 import SwiftUI
 
 struct ContentView: View {
+    struct DiningHall {
+        let name: String
+        let status: String
+        let hours: String
+        let image: String
+    }
+    
+    let diningHalls = [
+        DiningHall(name: "1920 Commons", status: "OPEN", hours: "11 - 3 | 5 - 8", image: "commons")
+    
+    ]
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack(alignment: .leading) {
+            Text("SUNDAY, FEBRUARY 4")
+                .font(.system(size: 11, weight: .heavy))
+                .foregroundColor(.gray)
+                .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/, alignment: .leading)
+            
+            Text("Dining Halls")
+                .font(.system(size: 28, weight: .bold))
+                .padding(.bottom, 12)
+
+            List {
+                
+            }
         }
-        .padding()
+        .padding(.horizontal)
     }
 }
 
